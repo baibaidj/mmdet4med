28889ba5e88faf27cbb8a44f3c2c1f353761d257
dejuns <dejuns@keyamedical.com>
2021-10-26 15:47:42 +0800

    minor

--- mmdet/core/utils/__init__.py
+++ mmdet/core/utils/__init__.py
@@ -5,7 +5,7 @@ from .misc import (center_of_mass, flip_tensor, generate_coordinate,
                    mask2ndarray, multi_apply, unmap)
 from .swa_hook import SWAHook
 
-
+print_tensor = lambda n, x: print(n, type(x), x.dtype, x.shape, x.min(), x.max())
 __all__ = [
     'allreduce_grads', 'DistOptimizerHook', 'reduce_mean', 'multi_apply',
     'unmap', 'mask2ndarray', 'flip_tensor', 'all_reduce_dict',
